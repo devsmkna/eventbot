0549ddc94a362f7d6fe74d264f884fd8a9728d0c
Davide Visco <davidevisco@email.com>
2024-07-27 15:55:36 +0200

    feat(event, interaction): add event type, schema and model

---
+++ src/types/event.d.ts
@@ -0,0 +1,45 @@
+import { TInteraction } from "./interaction";
+
+export type TEvent = {
+  /**
+   * Unique identifier of the event
+   */
+  id: string;
+  /**
+   * Telegram user id of the creator of the event
+   */
+  creator: number;
+  /**
+   * Name of the event
+   * @minLenght 5
+   * @maxLenght 50
+   */
+  name: string;
+  /**
+   * Description of the event, markdown formatted
+   * @maxLenght 140
+   */
+  description?: string;
+  /**
+   * List of coordinates of the event
+   */
+  locations?: Array<[number, number]>;
+  /**
+   * List of Telegram user ids with the interaction to the event
+   */
+  participants: Array<TInteraction>;
+  /**
+   * List of dates of the event
+   */
+  dates: Array<[Date] | [Date, Date]>;
+  /**
+   * Date of creation of the event
+   */
+  createdAt: Date;
+  /**
+   * Date of update of the event
+   */
+  updatedAt: Date;
+};
+
+export type MockTEvent = Omit<TEvent, "id" | "createdAt" | "updatedAt">;
---
+++ src/types/interaction.ts
@@ -0,0 +1,16 @@
+export type TInteraction = {
+  /**
+   * Telegram user id
+   */
+  user: number;
+  /**
+   * Type of interaction with the event
+   */
+  interaction: (typeof interactions)[number];
+};
+
+export const interactions = [
+  "partecipate",
+  "maybe",
+  "not partecipate",
+] as const;
