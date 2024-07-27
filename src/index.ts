375a5caa6aac22fc987674cef4c9bb4563244600
Davide Visco <davidevisco@email.com>
2024-07-28 01:01:58 +0200

    chore: add logger (#1)
    * chore: add logger
    
    * chore: add file logging

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import { Telegraf } from "telegraf";
-import { BOT_TOKEN, DB_URL } from "./config/env";
 import { connect } from "mongoose";
+import { Telegraf } from "telegraf";
+import { BOT_TOKEN, DB_URL, logger } from "./config/env";
 
 const init = async () => {
   try {
     await connect(DB_URL);
-    console.log("Connected to database");
+    logger.info("Connected to database");
     const bot = new Telegraf(BOT_TOKEN);
 
     bot.launch();
+    logger.info("Bot started");
 
-    process.once("SIGINT", () => bot.stop("SIGINT"));
-    process.once("SIGTERM", () => bot.stop("SIGTERM"));
+    process.once("SIGINT", () => {
+      bot.stop("SIGINT");
+      logger.info("SIGINT: Bot stopped");
+    });
+    process.once("SIGTERM", () => {
+      bot.stop("SIGTERM");
+      logger.info("SIGTERM: Bot stopped");
+    });
   } catch (error) {
-    console.error(error);
+    logger.error(error);
   }
 };
 
